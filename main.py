e69e843afaede750f6ffe548e94ffbac6b145a1b
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-26 00:05:09 +0000

    1.2

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ Készíts egy programot, amely a felhasználó által megadott mondatról a mond
 1.2 Feladat
 Az előbbi programot módosítsd úgy, hogy újabb és újabb mondatot kérjen be a program (amig a felhasználó csak egy ENTER-t nem üt), majd állapítsa meg, és írja ki mineden egyes alkalommal a mondat fajtáját!
 '''
-beker = input('mondat: ')
-if beker[-1] == '?':
-    print('kérdőmondat')
 
-elif beker[-1] == '.':
-    print('kijelentő mondat')
+while True:
+        
+    beker = input('mondat: ')
+    if beker == '':
+        break
+    elif beker[-1] == '?':
+        print('kérdőmondat')
 
-elif beker[-1] == '!' and 'Bárcsak' in beker or 'bárcsak' in beker:
-    print('óhajtó')
+    elif beker[-1] == '.':
+        print('kijelentő mondat')
 
-elif beker[-1] == '!' and 'Bárcsak' not in beker or 'bárcsak'  not in  beker :
-    print('Felkialto mondat')
+    elif beker[-1] == '!' and 'Bárcsak' in beker or 'bárcsak' in beker:
+        print('óhajtó')
+
+    elif beker[-1] == '!' and 'Bárcsak' not in beker or 'bárcsak'  not in  beker :
+        print('Felkialto mondat')
